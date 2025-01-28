5946bd6e9b83e222de23a06a94abc1df033b9298
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-01-28 14:17:49 +0300

    Update StockController.java

--- src/main/java/com/s5/pharmacie_backoffice/controllers/StockController.java
+++ src/main/java/com/s5/pharmacie_backoffice/controllers/StockController.java
@@ -1,10 +1,28 @@
 package com.s5.pharmacie_backoffice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.s5.pharmacie_backoffice.services.MedicamentFicheService;
 
 @RequestMapping("/stocks")
 @Controller
 public class StockController {
+    @Autowired
+    private MedicamentFicheService medicamentFicheService;
     
+    @GetMapping("/suivi-form")
+    public ModelAndView recherche() {
+        ModelAndView modelAndView=new ModelAndView("template");
+        modelAndView.addObject("page","stock/suivi-form");
+        modelAndView.addObject("fiches", medicamentFicheService.recupererMedicamentFiches());
+        return modelAndView;
+    }
+
+
+    
+
+
 }
